2fe97e9185c7818b834165e1bd3da28c6b85a8b2
Miroslav Douda <Miramirinda@outlook.cz>
2024-05-22 12:33:59 +0200

    fail

--- API/Weather Forecast/script.js
+++ API/Weather Forecast/script.js
@@ -1,9 +1,16 @@
-const api_url = "https://api.weatherapi.com/v1/current.json?key=7bda45a82ec34c28bfc90449242205&q=Ústí nad Labem&aqi=no";
+const api_url = "https://api.weatherapi.com/v1/history.json?key=7bda45a82ec34c28bfc90449242205&q=Ústí nad Labem&dt=2024-05-22";
 
 
 async function getWeather() {
     const jsonFile = await fetch(api_url); // Fetch the JSON file
     const data = await jsonFile.json(); // convert the JSON file to a JavaScript object
+    console.log(data.forecast.forecastday[0].date);
+    
+
+    (data.forecast.forecastday[0].forecast.hour).forEach(hour => {
+        console.log("a");
+    });
+
     createNewCity(data.location.name, data.current.temp_c, data.current.temp_f, data.current.last_updated);
 
 }
